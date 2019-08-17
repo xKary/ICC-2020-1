a58d1787f41fd7c7d923d6c624c25f3e69b9de7b
xKary <karyxprado@ciencias.unam.mx>
2019-08-16 22:34:06 -0500

    Practica 2

--- practica01/Main.java
+++ practica01/Main.java
@@ -3,6 +3,7 @@ public class Main{
   public static void main(String[] args) {
     System.out.println("openjdk 11.0.2 2019-01-15");
     System.out.println("OpenJDK Runtime Environment 18.9 (build 11.0.2+9)");
-    System.out.println("OpenJDK 64-Bit Server VM 18.9 (build 11.0.2+9, mixed mode)");
+    System.out.println("OpenJDK 64-Bit Server VM 18.9 (build 11.0.2+9, mixed mode) \n");
+    System.out.println("Linux xkary-TP501UQ 5.0.0-23-generic #24~18.04.1-Ubuntu SMP Mon Jul 29 16:12:28 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux");
   }
 }
