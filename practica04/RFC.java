0bf1fa94db64f80c64398fc478c503e24dac5f1d
xKary <karyxprado@ciencias.unam.mx>
2019-08-22 18:46:51 -0500

    p4

--- practica04/RFC.java
+++ practica04/RFC.java
@@ -4,49 +4,42 @@ public class RFC {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String nombreCompleto, nomComp , nombre, nacimiento, apellidoPaterno, apellidoMaterno,rfc, dia,mes, año;
+		String nombreCompleto,nombre,nacimiento,apellidoPaterno,apellidoMaterno,rfc,dia,mes,año;
 		int separador;
-		
+
 		//Pedir datos
 		System.out.println("Introduce tu nombre completo:");
 		nombreCompleto = scanner.nextLine();
 		System.out.println("Introduce tu fecha de nacimiento en formato dd/mm/aaaa:");
 		nacimiento = scanner.nextLine();
-		
+
 		//Separar nombre y apellidos
-		nomComp = nombreCompleto;
 		//Nombre
 		separador = nombreCompleto.indexOf(" ");
 		nombre = nombreCompleto.substring(0,separador);
-		nombreCompleto = nombreCompleto.substring(separador+1, nombreCompleto.length());
 		//Apellido paterno
-		separador = nombreCompleto.indexOf(" ");
-		apellidoPaterno = nombreCompleto.substring(0,separador);
+		apellidoPaterno = nombreCompleto.substring(separador + 1,nombreCompleto.lastIndexOf(" "));
 		//Apellido materno
-		nombreCompleto = nombreCompleto.substring(separador+1,nombreCompleto.length());
-		apellidoMaterno = nombreCompleto;
+		separador = nombreCompleto.lastIndexOf(" ");
+		apellidoMaterno = nombreCompleto.substring(separador + 1,nombreCompleto.length());
 
 		//Separar la fecha
 		//Día
-		separador = nacimiento.indexOf("/");
-		dia = nacimiento.substring(0,separador);
+		dia = nacimiento.substring(0,2);
 		//Mes
-		nacimiento = nacimiento.substring(separador+1,nacimiento.length());
-		separador = nacimiento.indexOf("/");
-		mes = nacimiento.substring(0,separador);;
+		mes = nacimiento.substring(3,5);
 		//Año
-		nacimiento =  nacimiento.substring(separador+1, nacimiento.length());
-		año = nacimiento.substring(2,nacimiento.length());
-		
+		año = nacimiento.substring(8,nacimiento.length());
+
 		//Armar el RFC
 		rfc = apellidoPaterno.substring(0,2);
-		rfc += apellidoMaterno.substring(0,1);
-		rfc += nombre.substring(0,1);
+		rfc += apellidoMaterno.charAt(0);
+		rfc += nombre.charAt(0);
 		rfc = rfc.toUpperCase();
 		rfc += año + mes + dia;
-	  			      
-		System.out.println("EL RFC de " + nomComp + " es: " + rfc);	
-		
+
+		System.out.println("EL RFC de " + nombreCompleto + " es: " + rfc);
+
 	}
 
 }
