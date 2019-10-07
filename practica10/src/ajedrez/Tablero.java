9e28ac4eea11188f333eb10bed98c278ed5ceff3
xKary <karyxprado@ciencias.unam.mx>
2019-10-07 18:48:37 -0500

    Tamaño

--- practica10/src/ajedrez/Tablero.java
+++ practica10/src/ajedrez/Tablero.java
@@ -9,8 +9,7 @@ public class Tablero extends PApplet {
 
   @Override
   public void settings(){
-    size(displayHeight - 200 ,displayHeight - 200);
-    //fullScreen();
+    size(displayHeight - 100 ,displayHeight - 100);
   }
   @Override
   public void setup(){
@@ -18,12 +17,11 @@ public class Tablero extends PApplet {
   }
   @Override
   public void draw(){
-    float alto = (displayHeight - 200) / 8;
-    float ancho = alto;//(displayWidth - 200) / 8;
+    float lado = (displayHeight - 100) / 8;
     for (int i = 0;i < 8; i++) {
       for (int j = 0; j < 8;j++) {
         fill(((j%2 != 0 && i%2 !=0 )||(j%2 == 0 && i%2 ==0 ))? 0xFFFFFF :0x000000);
-        rect(j*ancho,i*alto,ancho,alto);
+        rect(j*lado,i*lado,lado,lado);
       }
     }
 
