d6facf0f3e8a647732fdcb81dace77ca614d3b94
xKary <karyxprado@ciencias.unam.mx>
2019-10-22 19:09:59 -0500

    Peon

---
+++ practica12/src/main/java/ajedrez/piezas/Posicion.java
@@ -0,0 +1,51 @@
+package ajedrez.piezas;
+
+public class Posicion {
+
+  private int fila;
+  private int columna;
+
+  /**
+   * Constructor
+   * @param fila
+   * @param columna
+  */
+  public Posicion(int fila, int columna){
+    this.fila = fila;
+    this.columna = columna;
+  }
+
+  /**
+   * Metodo que regresa la fila
+   * @return int -fila de la posicion
+  */
+  public int obtenerFila(){
+    return fila;
+  }
+
+  /**
+   * Metodo que regresa la columna
+   * @return int -columna de la posicion
+  */
+  public int obtenerColumna(){
+    return columna;
+  }
+
+  /**
+   * Metodo que determina si dos posiones son iguales
+   * @param obj - objeto con el que se comparara
+   * @return boolean -true si son iguales, false en otro caso
+  */
+  @Override
+  public boolean equals(Object obj){
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    Posicion otra = (Posicion) obj;
+    return fila == otra.fila && columna == otra.columna;
+  }
+
+}
---
+++ practica12/src/main/java/ajedrez/piezas/pruebaPeon.java
@@ -0,0 +1,54 @@
+package ajedrez.piezas;
+import java.util.List;
+/**
+ * Programa para probar la lista de jugadas válidas de la pieza Peon
+ * @author Prado Oropeza Karina Vianey
+*/
+public class pruebaPeon {
+  public static void main(String[] args) {
+
+    Peon [] negros = new Peon [4];
+    Peon [] blancos = new Peon[4];
+
+    negros[0] = new Peon(Color.NEGRO,new Posicion(1,1));
+    negros[1] = new Peon(Color.NEGRO,new Posicion(2,3));
+    negros[2] = new Peon(Color.NEGRO,new Posicion(1,5));
+    negros[3] = new Peon(Color.NEGRO,new Posicion(2,7));
+
+    blancos[0] = new Peon(Color.BLANCO,new Posicion(6,1));
+    blancos[1] = new Peon(Color.BLANCO,new Posicion(5,3));
+    blancos[2] = new Peon(Color.BLANCO,new Posicion(4,5));
+    blancos[3] = new Peon(Color.BLANCO,new Posicion(6,7));
+
+    System.out.println("PEONES NEGROS");
+    for (Peon peonNegro: negros) {
+      //Jugadas validas para cada elemento de negros
+      List<Posicion> jugadas = peonNegro.obtenerJugadasLegales();
+      //Posicion de peon
+      System.out.print("---Jugas para el peon en ");
+      System.out.println("(" + peonNegro.obtenerPosicion().obtenerFila() + ","
+                             + peonNegro.obtenerPosicion().obtenerColumna() + ")--- \n");
+      //Mostrar las jugadas
+      for (Posicion jugada: jugadas) {
+        System.out.println("Fila: " + jugada.obtenerFila() + "\t" +
+                           "Columna: " + jugada.obtenerColumna() + "\n");
+      }
+    }
+
+    System.out.println("PEONES BLANCOS");
+    for (Peon peonBlanco: blancos) {
+      //Jugadas válidas para cada peon blanco
+      List<Posicion> jugadas = peonBlanco.obtenerJugadasLegales();
+      //Posicion del peon
+      System.out.print("---Jugas para el peon en ");
+      System.out.println("(" + peonBlanco.obtenerPosicion().obtenerFila() + ","
+                             + peonBlanco.obtenerPosicion().obtenerColumna() + ")--- \n");
+      //Mostrar jugadas
+      for (Posicion jugada: jugadas) {
+        System.out.println("Fila: " + jugada.obtenerFila() + "\t" +
+                           "Columna: " + jugada.obtenerColumna() + "\n");
+      }
+    }
+
+  }
+}
