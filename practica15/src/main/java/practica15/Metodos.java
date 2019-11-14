6a2dcd38613a6bfcfcd4d983a0dfc764175fcea7
xKary <karyxprado@ciencias.unam.mx>
2019-11-13 19:00:32 -0600

    3

--- practica15/src/main/java/practica15/Metodos.java
+++ practica15/src/main/java/practica15/Metodos.java
@@ -3,21 +3,43 @@ package practica15;
 import java.util.stream.Stream;
 import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.Comparator;
-
+/**
+ * Clase que utiliza la interfaz Stream
+ * @author Karina Prado Oropeza
+*/
 public class Metodos{
-
+  /**
+   * Método que ordena cadenas por su tamaño,
+   * del más largo al más corto.
+   * @param list - lista con las cadenas a ordenar
+   * @return Stream<String> - cadenas ordenadas
+  */
   public static Stream<String> ordenar(List<String> list){
     return list.stream()
                .sorted(Comparator.comparingInt(String::length));
   }
-
-  public static Stream<Integer> descartar(List<Integer> list){
+  /**
+   * Método que descarta todos los elementos
+   * menores a cero o mayores a nueve,y regresa un Stream<String>
+   * con la representación en español de cada dı́gito
+   * @param list - lista con números a verificar
+   * @return Stream<String> - representación en español de cada dı́gito
+  */
+  public static Stream<String> descartar(List<Integer> list){
+    //Arreglo con el nombre de los digitos
+    String[] numeros = {"cero","uno","dos","tres","cuatro",
+                        "cinco","seis","siete","ocho","nueve"};
     return list.stream()
-               .filter(n -> (n >= 0 && n < 10));
+               .filter(n -> (n >= 0 && n < 10))
+               .map(n -> numeros[n]);
   }
-
+  /**
+   * Método que recibe una cadena s y devuelve un Stream<String>
+   * infinito con repeticiones de s
+   * @param s - cadena a repetir
+   * @return Stream<String> - repeticiones
+  */
   public static Stream<String> repetir(String s){
     return Stream.generate(new Supplier<String>(){
       int i = 0;
@@ -26,7 +48,5 @@ public class Metodos{
       }
     });
   }
-
-
 
 }
--- practica15/src/main/java/practica15/Prueba.java
+++ practica15/src/main/java/practica15/Prueba.java
@@ -2,19 +2,27 @@ package practica15;
 
 import java.util.stream.Stream;
 import java.util.List;
-import java.util.function.Supplier;
-
+/**
+ * Clase que prueba los metodos de la clase Metodos
+ * @author Karina Prado Oropeza
+*/
 public class Prueba{
   public static void main(String[] args) {
-    
+
+    System.out.println("\nOrdenar palabras por su longitud");
     List<String> cadenas = List.of("squeezy", "easy", "", "peasy","lemon");
+    //utilizar el metodo ordenar, para ordenar "cadenas" y mostrarlo en pantalla
     Metodos.ordenar(cadenas)
            .forEach(System.out::println);
 
+    System.out.println("\nNúmeros entre 0 y 9");
     List<Integer> lista = List.of(12,3,4,5,89,0,2,-20);
+    //utilizar el metodo descartar en lista y mostrarlo en pantalla
     Metodos.descartar(lista)
            .forEach(System.out::println);
 
+    System.out.println("\nRepetir una palabra");
+    //utilizar el metodo repetir y mostrar el resultado en pantalla
     Metodos.repetir("Hola")
            .limit(10)
            .forEach(System.out::println);
