7b910a447687ddac4c18651b18727a598b256318
xKary <karyxprado@ciencias.unam.mx>
2019-11-19 12:35:04 -0600

    13

--- practica13/src/main/java/ajedrez/piezas/pruebaDama.java
+++ practica13/src/main/java/ajedrez/piezas/pruebaDama.java
@@ -7,22 +7,8 @@ import java.util.List;
 public class pruebaDama {
   public static void main(String[] args) {
 
-    Peon [] negros = new Peon [5];
-    Peon [] blancos = new Peon[5];
     Dama damaB = new Dama (Color.BLANCO,new Posicion(7,3));
     Dama damaN = new Dama (Color.NEGRO,new Posicion(0,3));
-
-    negros[0] = new Peon(Color.NEGRO,new Posicion(1,0));
-    negros[1] = new Peon(Color.NEGRO,new Posicion(1,1));
-    negros[2] = new Peon(Color.NEGRO,new Posicion(1,5));
-    negros[3] = new Peon(Color.NEGRO,new Posicion(1,6));
-    negros[3] = new Peon(Color.NEGRO,new Posicion(1,7));
-
-    blancos[0] = new Peon(Color.BLANCO,new Posicion(6,0));
-    blancos[1] = new Peon(Color.BLANCO,new Posicion(6,1));
-    blancos[2] = new Peon(Color.BLANCO,new Posicion(6,5));
-    blancos[3] = new Peon(Color.BLANCO,new Posicion(6,6));
-    blancos[4] = new Peon(Color.BLANCO,new Posicion(6,7));
 
     List<Posicion> jugadas = damaB.obtenerJugadasLegales();
     System.out.println("--- Jugas dama blanca ---");
