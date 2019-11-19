ede6daa58b810be0efdcb8ba9524a8246ea50e33
xKary <karyxprado@ciencias.unam.mx>
2019-11-19 12:31:01 -0600

    ||1211

--- practica12/src/main/java/ajedrez/piezas/Pieza.java
+++ practica12/src/main/java/ajedrez/piezas/Pieza.java
@@ -2,9 +2,10 @@ package ajedrez.piezas;
 
 import java.util.List;
 import ajedrez.piezas.Posicion;
+
 /**
  * Clase abstracta de la que heredaran las piezas del ajedrez
-*/
+ */
 public abstract class Pieza {
 
   private Color color;
@@ -12,10 +13,11 @@ public abstract class Pieza {
 
   /**
    * Constructor
-   * @param color - color que se asignara
+   * 
+   * @param color    - color que se asignara
    * @param posicion - posicion que se asignara
-  */
-  public Pieza (Color color, Posicion posicion){
+   */
+  public Pieza(Color color, Posicion posicion) {
     this.color = color;
     this.posicion = posicion;
   }
@@ -24,35 +26,39 @@ public abstract class Pieza {
 
   /**
    * Metodo que regresa el color de la pieza
+   * 
    * @return Color -color de la pieza
-  */
-  public Color obtenerColor(){
-     return color;
+   */
+  public Color obtenerColor() {
+    return color;
   }
 
   /**
    * Metodo que regresa la posicion de la pieza
+   * 
    * @return Posicion -posicion de la pieza
-  */
-  public Posicion obtenerPosicion(){
-     return posicion;
+   */
+  public Posicion obtenerPosicion() {
+    return posicion;
   }
 
   /**
    * Metodo que le asigna una posion a la pieza
+   * 
    * @param posicion - posicion que se asignara
    */
-  public void asignarPosicion(Posicion posicion){
+  public void asignarPosicion(Posicion posicion) {
     this.posicion = posicion;
   }
 
   /**
    * Metodo que determina si dos Piezas son iguales
+   * 
    * @param obj - objeto con el que se comparara
    * @return boolean -true si son iguales, false en otro caso
-  */
+   */
   @Override
-  public boolean equals(Object obj){
+  public boolean equals(Object obj) {
     if (this == obj) {
       return true;
     }
--- practica12/src/main/java/ajedrez/piezas/Posicion.java
+++ practica12/src/main/java/ajedrez/piezas/Posicion.java
@@ -7,37 +7,41 @@ public class Posicion {
 
   /**
    * Constructor
+   * 
    * @param fila
    * @param columna
-  */
-  public Posicion(int fila, int columna){
+   */
+  public Posicion(int fila, int columna) {
     this.fila = fila;
     this.columna = columna;
   }
 
   /**
    * Metodo que regresa la fila
+   * 
    * @return int -fila de la posicion
-  */
-  public int obtenerFila(){
+   */
+  public int obtenerFila() {
     return fila;
   }
 
   /**
    * Metodo que regresa la columna
+   * 
    * @return int -columna de la posicion
-  */
-  public int obtenerColumna(){
+   */
+  public int obtenerColumna() {
     return columna;
   }
 
   /**
    * Metodo que determina si dos posiones son iguales
+   * 
    * @param obj - objeto con el que se comparara
    * @return boolean -true si son iguales, false en otro caso
-  */
+   */
   @Override
-  public boolean equals(Object obj){
+  public boolean equals(Object obj) {
     if (this == obj) {
       return true;
     }
