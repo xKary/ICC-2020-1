097b45764b463c81980c565c472caa912fa2d6e7
xKary <karyxprado@ciencias.unam.mx>
2019-11-19 12:38:28 -0600

    i

--- practica15/src/main/java/practica15/Metodos.java
+++ practica15/src/main/java/practica15/Metodos.java
@@ -6,65 +6,66 @@ import java.util.function.Supplier;
 import java.util.Comparator;
 import java.util.stream.IntStream;
 import java.util.stream.Collectors;
+
 /**
  * Clase que utiliza la interfaz Stream
+ * 
  * @author Karina Prado Oropeza
-*/
-public class Metodos{
+ */
+public class Metodos {
   /**
-   * Método que ordena cadenas por su tamaño,
-   * del más largo al más corto.
+   * Método que ordena cadenas por su tamaño, del más largo al más corto.
+   * 
    * @param list - lista con las cadenas a ordenar
    * @return Stream<String> - cadenas ordenadas
-  */
-  public static Stream<String> ordenar(List<String> list){
-    return list.stream()
-               .sorted(Comparator.comparingInt(String::length));
+   */
+  public static Stream<String> ordenar(List<String> list) {
+    return list.stream().sorted(Comparator.comparingInt(String::length));
   }
+
   /**
-   * Método que descarta todos los elementos
-   * menores a cero o mayores a nueve,y regresa un Stream<String>
-   * con la representación en español de cada dı́gito
+   * Método que descarta todos los elementos menores a cero o mayores a nueve,y
+   * regresa un Stream<String> con la representación en español de cada dı́gito
+   * 
    * @param list - lista con números a verificar
    * @return Stream<String> - representación en español de cada dı́gito
-  */
-  public static Stream<String> descartar(List<Integer> list){
-    //Arreglo con el nombre de los digitos
-    String[] numeros = {"cero","uno","dos","tres","cuatro",
-                        "cinco","seis","siete","ocho","nueve"};
-    return list.stream()
-               .filter(n -> (n >= 0 && n < 10))
-               .map(n -> numeros[n]);
+   */
+  public static Stream<String> descartar(List<Integer> list) {
+    // Arreglo con el nombre de los digitos
+    String[] numeros = { "cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve" };
+    return list.stream().filter(n -> (n >= 0 && n < 10)).map(n -> numeros[n]);
   }
+
   /**
-   * Método que recibe una cadena s y devuelve un Stream<String>
-   * infinito con repeticiones de s
+   * Método que recibe una cadena s y devuelve un Stream<String> infinito con
+   * repeticiones de s
+   * 
    * @param s - cadena a repetir
    * @return Stream<String> - repeticiones
-  */
-  public static Stream<String> repetir(String s){
-    return Stream.generate(new Supplier<String>(){
+   */
+  public static Stream<String> repetir(String s) {
+    return Stream.generate(new Supplier<String>() {
       int i = 0;
-      public String get(){
+
+      public String get() {
         return s.repeat(i++);
       }
     });
   }
+
   /**
-   * Método que recibe un entero y regresa una lista con 50 múltiplos
-   * aleatorios
+   * Método que recibe un entero y regresa una lista con 50 múltiplos aleatorios
+   * 
    * @param k - entero
    * @return List<String> -lista con 50 multiplos de k
-  */
-  public static List<String> multiplos(int k){
-    //Generar IntStream con números entre el 0 y 1000
-    IntStream multi = IntStream.generate(()
-                     -> { return (int) (Math.random() * 1000); });
+   */
+  public static List<String> multiplos(int k) {
+    // Generar IntStream con números entre el 0 y 1000
+    IntStream multi = IntStream.generate(() -> {
+      return (int) (Math.random() * 1000);
+    });
 
-    return multi.filter(m -> (m%k == 0))
-                .limit(50)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.toList());
+    return multi.filter(m -> (m % k == 0)).limit(50).mapToObj(String::valueOf).collect(Collectors.toList());
   }
 
 }
--- practica15/src/main/java/practica15/Prueba.java
+++ practica15/src/main/java/practica15/Prueba.java
@@ -2,34 +2,32 @@ package practica15;
 
 import java.util.stream.Stream;
 import java.util.List;
+
 /**
  * Clase que prueba los metodos de la clase Metodos
+ * 
  * @author Karina Prado Oropeza
-*/
-public class Prueba{
-  public static void main(String[] args) {
+ */
+public class Prueba {
+       public static void main(String[] args) {
 
-    System.out.println("\nOrdenar palabras por su longitud");
-    List<String> cadenas = List.of("squeezy", "easy", "", "peasy","lemon");
-    //utilizar el metodo ordenar, para ordenar "cadenas" y mostrarlo en pantalla
-    Metodos.ordenar(cadenas)
-           .forEach(System.out::println);
+              System.out.println("\nOrdenar palabras por su longitud");
+              List<String> cadenas = List.of("squeezy", "easy", "", "peasy", "lemon");
+              // utilizar el metodo ordenar, para ordenar "cadenas" y mostrarlo en pantalla
+              Metodos.ordenar(cadenas).forEach(System.out::println);
 
-    System.out.println("\nNúmeros entre 0 y 9");
-    List<Integer> lista = List.of(12,3,4,5,89,0,2,-20);
-    //utilizar el metodo descartar en lista y mostrarlo en pantalla
-    Metodos.descartar(lista)
-           .forEach(System.out::println);
+              System.out.println("\nNúmeros entre 0 y 9");
+              List<Integer> lista = List.of(12, 3, 4, 5, 89, 0, 2, -20);
+              // utilizar el metodo descartar en lista y mostrarlo en pantalla
+              Metodos.descartar(lista).forEach(System.out::println);
 
-    System.out.println("\nRepetir una palabra");
-    //utilizar el metodo repetir y mostrar el resultado en pantalla
-    Metodos.repetir("Hola")
-           .limit(10)
-           .forEach(System.out::println);
+              System.out.println("\nRepetir una palabra");
+              // utilizar el metodo repetir y mostrar el resultado en pantalla
+              Metodos.repetir("Hola").limit(10).forEach(System.out::println);
 
-    System.out.println("\nMultiplos de un número (3)");
-    //utilizar el metodo multiplos, para obtener 50 multiplos de 3
-    Metodos.multiplos(3)
-           .forEach(System.out::println);;
-  }
+              System.out.println("\nMultiplos de un número (3)");
+              // utilizar el metodo multiplos, para obtener 50 multiplos de 3
+              Metodos.multiplos(3).forEach(System.out::println);
+              ;
+       }
 }
