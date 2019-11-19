85aa169dd2f2dd792df291636101e7577991319c
xKary <karyxprado@ciencias.unam.mx>
2019-11-19 12:36:40 -0600

    p14

--- practica14/src/main/java/practica14/LectorCSV.java
+++ practica14/src/main/java/practica14/LectorCSV.java
@@ -5,23 +5,26 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.LinkedList;
+
 /**
  * Clase para leer los datos de un archivo LectorCSV
+ * 
  * @author Prado Oropeza Karina Vianey
-*/
+ */
 public class LectorCSV {
   /**
    * Método que regresa una lista con todas las lineas del archivo
+   * 
    * @param ruta -ruta del archivo
    * @return List<String[]> - lista con los datos del archivo
-  */
-  public static List<String[]> leer (String ruta) throws IOException{
+   */
+  public static List<String[]> leer(String ruta) throws IOException {
 
     List<String> lineas = Files.readAllLines(Paths.get(ruta));
     LinkedList<String[]> separados = new LinkedList<>();
 
-    //Separar cada linea del archivo y obtener arreglos
-    for (String linea: lineas) {
+    // Separar cada linea del archivo y obtener arreglos
+    for (String linea : lineas) {
       String[] coor = linea.split(",");
 
       if (!coor[0].trim().isBlank() && !coor[1].trim().isBlank())
@@ -29,9 +32,10 @@ public class LectorCSV {
 
     }
 
-    //Quitar el primer elemento de separados, ya que son los titulos de las columanas
+    // Quitar el primer elemento de separados, ya que son los titulos de las
+    // columanas
     separados.remove(0);
-    
+
     return separados;
   }
 
